17a57ede7fe77c22f9df3fb27ed3cb56e24fca8a
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2025-06-01 18:26:10 -0400

    crud completo

--- src/routes/totem_router.ts
+++ src/routes/totem_router.ts
@@ -1,25 +1,34 @@
 import { Router } from 'express';
-import { AlunoRouter } from './aluno_router';
-import { CursoRouter } from './curso_router';
-import { MatriculaRouter } from './matricula_router';
 import { AuthRouter } from './auth-route';
+import { AlunoRouter } from './aluno_router';
+import { BoletoRouter } from './boleto_router';
+import { CursoRouter } from './curso_router';
+import { DocumentoCursoRouter } from './documento_curso_router';
+import { DocumentoMatriculaRouter } from './documento_matricula_router';
+import { MatriculaRouter } from './matricula_router';
 
 // import { UploadRouter } from './upload-routes';
 
 const router = Router();
 
 // Instanciando os routers
-const alunoRouter = new AlunoRouter();
-const cursoRouter = new CursoRouter();
-const matriculaRouter = new MatriculaRouter();
 const authRouter = new AuthRouter();
+const alunoRouter = new AlunoRouter();
+const boletoRouter = new BoletoRouter();
+const cursoRouter = new CursoRouter();
+const documentocursoRouter = new DocumentoCursoRouter();
+const documentomatriculaRouter = new DocumentoMatriculaRouter();
+const matriculaRouter = new MatriculaRouter();
 
 
 // Registrando as rotas
-router.use('/alunos', alunoRouter.router);
-router.use('/cursos', cursoRouter.router);
-router.use('/matriculas', matriculaRouter.router);
 router.use('/auth', authRouter.router);
+router.use('/alunos', alunoRouter.router);
+router.use('/boletos', boletoRouter.router);
+router.use('/cursos', cursoRouter.router);
+router.use('/doc_curso', documentocursoRouter.router);
+router.use('/doc_matricula', documentomatriculaRouter.router);
+router.use('/matriculas', matriculaRouter.router);
 
 
 
